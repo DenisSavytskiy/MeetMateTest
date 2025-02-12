9f474800bc935d295b8b291ac70f9688adbce775
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2025-02-12 21:58:23 +0200

    third commit

--- index.js
+++ index.js
@@ -13,6 +13,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || "localhost";
-app.listen(PORT, HOST, () => console.log(`Сервер запущено: http://${HOST}:${PORT}/API.html`));
+const PORT = process.env.PORT || 12424;
+app.listen(PORT, 'localhost', () => console.log(`Сервер запущено: http://localhost:${PORT}/API.html`));
